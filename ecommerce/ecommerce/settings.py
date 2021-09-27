ebac1643869c06a419bc7ac22f8f06bac0b844d9
GabrielBunea123 <76706013+GabrielBunea123@users.noreply.github.com>
2021-09-28 00:01:23 +0300

    done over

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -176,6 +176,6 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-djang_heroku.settings(locals())
+django_heroku.settings(locals())
 # prod_db = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
