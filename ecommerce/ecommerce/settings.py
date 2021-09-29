403ead47c309429c8609298bf1fb2ecec6c3f2c7
GabrielBunea123 <76706013+GabrielBunea123@users.noreply.github.com>
2021-09-30 00:13:11 +0300

    as

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -153,7 +153,7 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
