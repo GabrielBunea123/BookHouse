fa37c28241a9afea36182c1ac37410ab3adc97d2
GabrielBunea123 <76706013+GabrielBunea123@users.noreply.github.com>
2021-09-29 10:13:41 +0300

    asd

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -142,11 +142,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
