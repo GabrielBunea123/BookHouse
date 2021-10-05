462af811a081b86635c48bc324c4d1f25de35e1c
GabrielBunea123 <76706013+GabrielBunea123@users.noreply.github.com>
2021-10-05 16:26:48 +0300

    Force ssl redirect

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -198,4 +198,11 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
+
+
+if os.getcwd()=='/app':
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
+    SECURE_SSL_REDIRECT = True
+    DEBUG = False
+
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
