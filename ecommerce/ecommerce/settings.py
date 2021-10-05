5996321b5d0f4d8e0262c2421734fadabb275e11
GabrielBunea123 <76706013+GabrielBunea123@users.noreply.github.com>
2021-10-05 17:57:21 +0300

    as

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -199,6 +199,9 @@ AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 
 # if os.getcwd()=='/app':
 #     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
